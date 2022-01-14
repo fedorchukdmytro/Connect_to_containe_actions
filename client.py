0d4264ffbe18223914331748ad815603735548c8
fedorchukdmytro <fedorchuk.dmytro@ukr.net>
2022-01-14 17:07:11 +0000

    2

--- client.py
+++ client.py
@@ -17,7 +17,7 @@ class tc_one(aetest.Testcase):
     @aetest.test
     def client_launching(self):
         with open('output.json', 'w') as f:
-            client_process = subprocess.Popen(['iperf3', '-c', 'IP', '-J'], stdout=f,)
+            client_process = subprocess.Popen(['iperf3', '-c', '10.1.0.1', '-J'], stdout=f,)
         client_process.wait()
 
     @aetest.cleanup
