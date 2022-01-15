74fe36a00ec315eb749e81c2c3a2bb75f8d81811
fedorchukdmytro <fedorchuk.dmytro@ukr.net>
2022-01-15 19:18:00 +0000

    4[A[A

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ class tc_one(aetest.Testcase):
 
     @aetest.test
     def server_launching(self):
-       server = subprocess.Popen(['iperf3' , '-s', '--one-off'])
+       server = subprocess.Popen(['iperf3' , '-s', '--one-off', '-D'])
 
     @aetest.cleanup
     def clean_testcase(self):
