3460df976d0fc546a0e167813c3bc188399b129a
fedorchukdmytro <fedorchuk.dmytro@ukr.net>
2022-01-17 13:05:01 +0000

    123

--- server1.py
+++ server1.py
@@ -1,12 +1,13 @@
 import logging
 import subprocess
-
+import time
 logger = logging.getLogger(__name__)
 
 
 logger.info("Preparing the test")
   
 server = subprocess.Popen(['iperf3' , '-s', '--one-off'])
+time.sleep(300)
 
 logger.info("Pass testcase cleanup")
 
